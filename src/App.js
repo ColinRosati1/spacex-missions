c0f0410a37c7331c2467f4d4218c34ac23e296e6
Colin Rosati <colin.james.rosati@gmail.com>
2019-12-06 15:00:20 +0100

    add basic flexbox style with using mixin on history and launch component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
 
 import History from './components/History'
 import Launches from './components/Launches'
@@ -10,8 +10,10 @@ function App() {
       <header className="App-header">
         SPACE X MISSIONS
       </header>
-      <History/>
-      <Launches/>
+      <div className="app-body">
+        <History/>
+        <Launches/>
+      </div>
     </div>
   );
 }
