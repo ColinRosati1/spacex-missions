1a963e0ea570d4c12e563019adcdac68e8dacf59
Colin Rosati <colin.james.rosati@gmail.com>
2019-12-07 15:38:21 +0100

    add unit tests for HistoryItem component

--- src/components/tests/HistoryItem.test.js
+++ src/components/tests/HistoryItem.test.js
@@ -1,11 +1,48 @@
 import HistoryItem from '../HistoryItem';
 import React from 'react'
-import { create } from "react-test-renderer";
-import { render } from '@testing-library/react';
+import Enzyme, { shallow, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-describe("HistoryItem component", () => {
-    test('renders HistoryItem component', () => {
-        const item = render(<HistoryItem />);
-        expect(item).toBeTruthy()
+// test # of props
+// test length of div children
+
+
+Enzyme.configure({ adapter: new Adapter() }) // look up how to do this globally
+
+describe('historyItem component', () => {
+    it('should render hitoryitems component', () => {
+        const wrapper = shallow(
+            <div className="history-item">
+                <div className="history-item-title"></div>
+                <div className="history-item-details"></div>
+                <div className="history-item-event"></div>
+                <div className="history-item-link"><a href>more</a></div>
+            </div>
+        );
+        expect(wrapper).toMatchSnapshot();
     });
-})y
+
+    it('should expect props to be valid', () => {
+        const it = mount(<HistoryItem/>);
+        expect(it.props().title).not.toBeNull();
+        expect(it.props().details).not.toBeNull();
+        expect(it.props().event_date_utc).not.toBeNull();
+        expect(it.props().links).not.toBeNull();
+    })
+
+    it('should have 5 divs', () => {
+        const wrapper = shallow( <HistoryItem/> )
+        const children = wrapper.find('div').children()
+        console.log(children.length)
+        expect(children.length).toBe(5);
+    })
+
+    
+    it('should be show contain text', () => {
+        const wrapper = shallow( <HistoryItem/> )
+        const text = wrapper.find('.history-item-link')
+        expect(text.text()).toBe('more')
+    })
+
+
+})
